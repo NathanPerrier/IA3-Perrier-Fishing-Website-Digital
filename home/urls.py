5e039a42446e23014cc66e7fc3fc78a888837400
nathan-perrier23 <nathanperrier138@gmail.com>
2024-05-11 23:01:42 +1000

    chore: Add pricing page and update navigation links

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("pricing/", views.pricing, name="pricing"),
+    
     path("dashboard/", views.dashboard, name="starter"),
 ]
