70a64d3d23ce5f429c4635f28a19af9ea10aa987
nathan-perrier23 <nathanperrier138@gmail.com>
2024-05-17 22:42:56 +1000

    chore: Update celery configuration and task logging in wildlifeAPI

--- apps/wildlifeAPI/celery.py
+++ apps/wildlifeAPI/celery.py
@@ -3,8 +3,9 @@ from celery.schedules import crontab
 from home.celery import app as app
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **_):
+    print(app)
     sender.add_periodic_task(5.0, clean_data.s(), name='Run every 5 minutes')
 
 @app.task
 def clean_data():
-    print("Running my task")
+    print("Running my task on ", app)
