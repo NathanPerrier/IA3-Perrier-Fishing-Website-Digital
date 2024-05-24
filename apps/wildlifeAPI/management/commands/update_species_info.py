b2b51831d130b2f1943088b294c54c8ae45c8144
nathan-perrier23 <nathanperrier138@gmail.com>
2024-05-24 13:52:33 +1000

    Update WildlifeSpeciesConservationStatus model to handle created flag

--- apps/wildlifeAPI/management/commands/update_species_info.py
+++ apps/wildlifeAPI/management/commands/update_species_info.py
@@ -1,19 +1,26 @@
 from django.core.management.base import BaseCommand, CommandError
-from apps.wildlifeAPI.models import WildlifeSpeciesInfo
+from apps.wildlifeAPI.models import WildlifeSpeciesInfo, WildlifeSpeciesImage, WildlifeSpeciesConservationStatus
 
 class Command(BaseCommand):
     help = 'Updates wildlife info data'
     
     def add_arguments(self, parser):
         parser.add_argument('--debug', action='store_true', help='Run in debug mode')
+        parser.add_argument('--clear', action='store_true', help='Clear Info Database')
 
 
     def handle(self, *args, **options):
         debug = options['debug']
+        clear = options['clear']
         
         try:
             self.stdout.write(self.style.WARNING('Updating wildlife info data...\nPlease Do Not Interrupt the Process!'))
             
+            if clear:
+                WildlifeSpeciesImage.objects.all().delete()
+                WildlifeSpeciesConservationStatus.objects.all().delete()
+                WildlifeSpeciesInfo.objects.all().delete()
+                
             WildlifeSpeciesInfo.update(debug=debug)
 
             self.stdout.write(self.style.SUCCESS('Successfully updated wildlife info data'))
