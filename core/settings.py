f19cb9ff7a6c2b1de5fcc8135e64f8c22a1657d8
nathan-perrier23 <nathanperrier138@gmail.com>
2024-06-30 18:18:41 +1000

    Update DEBUG mode to False in settings.py

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,7 @@ if not SECRET_KEY:
     SECRET_KEY = ''.join(random.choice( string.ascii_lowercase  ) for i in range( 32 ))
 
 # Enable/Disable DEBUG Mode
-DEBUG = True #str2bool(os.environ.get('DEBUG'))
+DEBUG = False #str2bool(os.environ.get('DEBUG'))
 
 # Hosts Settings
 ALLOWED_HOSTS = ['*']
